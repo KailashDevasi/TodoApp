f93717f9efc217a7579191eaf8ac860689c79890
kailash <kailashdevasi9481@outlook.com>
2025-11-24 18:48:35 +0530

    Added Render,s PostgresQL DB

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost/TodoApplicationDatabse'
+SQLALCHEMY_DATABASE_URL = 'postgresql://todoapp_db_a32a_user:a3Ayp85MmL7kRz1XioHy8JayhXyEwsYP@dpg-d4i5gaf5r7bs73c8fmp0-a/todoapp_db_a32a'
 
 engine = create_engine (SQLALCHEMY_DATABASE_URL)
 
