f36a63bdb1e23c402b790db56104813a3e9041e5
kailash <kailashdevasi9481@outlook.com>
2025-11-25 11:12:08 +0530

    Securely configure database using environment variables

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://todoapp_db_a32a_user:a3Ayp85MmL7kRz1XioHy8JayhXyEwsYP@dpg-d4i5gaf5r7bs73c8fmp0-a/todoapp_db_a32a'
 
-engine = create_engine (SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
-SessionLocal = sessionmaker (autocommit=False, autoflush=False, bind=engine)
+
+if SQLALCHEMY_DATABASE_URL:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./testdb.db"
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
